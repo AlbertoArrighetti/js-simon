f9d2153c8f4fbe0d785ef645c55a7d56923819c8
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-09 15:20:34 +0100

    gaining date and calculate time remaining

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 // Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di lunedi!
 // Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di lunedì mattina!
+
+// setto la data a lunedì mattina con new Date
+// 2024 = anno, 1 = Febbraio (i mesi iniziano da i = 0), 12 = giorno, 9 = ora, minuti e secondi 
+const targetDate = new Date (2024, 1, 12, 9, 0, 0);
+
+// data di oggi
+const now = new Date ();
+
+// visualizzo il tempo mancante fino a targetDate
+const timeRemaining = targetDate - now;
+
+console.log(timeRemaining);
+console.log("data di oggi ", now);
+console.log("lunedì ", targetDate);
+
